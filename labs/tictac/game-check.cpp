9851bdb343fb272439fd6cac14be009c19e03d47
Redstoneweewee <95556046+Redstoneweewee@users.noreply.github.com>
2025-01-15 18:40:51 -0800

    Removed parse error message for testing

--- labs/tictac/game-check.cpp
+++ labs/tictac/game-check.cpp
@@ -3,5 +3,8 @@
 #include "Move.h"
 
 int main() {
-
+    //TODO - while loop to parse each move
+    //TODO - Use each move to fill up the board
+    //TODO - print out game states while happening
+    //TODO - print errors when they occur
 }
