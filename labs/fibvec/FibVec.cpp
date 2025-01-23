d92c3c9d6327b6b56d15af716a9a43f5fd93445d
Redstoneweewee <95556046+Redstoneweewee@users.noreply.github.com>
2025-01-22 18:09:38 -0800

    Stubs

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -5,3 +5,44 @@
 
 
 // FibVec Function Implementations
+
+// Constructor and Destructor
+FibVec::FibVec() {
+    vectorPointer = new std::vector<int>(1);
+}
+FibVec::~FibVec() {
+    delete vectorPointer;
+}
+
+// Member Functions
+unsigned int FibVec::capacity() {
+    return 0;
+}
+
+unsigned int FibVec::count() {
+    return 0;
+}
+
+//can throw std::out_of_range exception
+void FibVec::insert(int value, unsigned int index) {
+
+}
+
+//can throw std::out_of_range exception
+int FibVec::lookup(unsigned int index) {
+    return 1;
+}
+
+//can throw std::underflow_error exception
+int FibVec::pop() {
+    return 1;
+}
+
+void FibVec::push(int value) {
+
+}
+
+//can throw std::out_of_range exception
+int FibVec::remove(unsigned int index) {
+    return 1;
+}
--- labs/fibvec/FibVec.h
+++ labs/fibvec/FibVec.h
@@ -3,17 +3,26 @@
 
 // This provides the size_t type:
 #include <cstddef>
+#include <vector>
 
 class FibVec {
-  // Member Variables
+    // Member Variables
+    std::vector<int>* vectorPointer;
+    // Helper Functions
 
-  // Helper Functions
+   public:
+    // Constructor and Destructor
+    FibVec();
+    ~FibVec();
 
-public:
-  // Constructor and Destructor
-
-  // Member Functions
-
+    // Member Functions
+    unsigned int capacity();
+    unsigned int count();
+    void insert(int value, unsigned int index); //can throw std::out_of_range exception
+    int lookup(unsigned int index); //can throw std::out_of_range exception
+    int pop(); //can throw std::underflow_error exception
+    void push(int value);
+    int remove(unsigned int index); //can throw std::out_of_range exception
 };
 
 #endif
