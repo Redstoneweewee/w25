822263e6d277f6d722eb37a29002052418f58a41
Redstoneweewee <95556046+Redstoneweewee@users.noreply.github.com>
2025-01-29 18:35:09 -0800

    added mCount to see if I can do that

--- labs/assort/List.cpp
+++ labs/assort/List.cpp
@@ -1,1 +1,18 @@
 #include "List.h"
+
+List::List() {
+    head = new Node;
+    mCount = 0;
+}
+
+List::List(const List& other) {
+    //TODO - make a new node from each node in other
+}
+
+List::~List() {
+    //TODO - go through to the end of the LL and remove all one by one
+}
+
+size_t List::count() const {
+    return mCount;
+}
