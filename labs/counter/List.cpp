5b4ed8fd28a468b5b4dc4df8b753604b34060feb
Redstoneweewee <95556046+Redstoneweewee@users.noreply.github.com>
2025-02-27 19:52:27 -0800

    First attempt at List.cpp

--- labs/counter/List.cpp
+++ labs/counter/List.cpp
@@ -1,3 +1,61 @@
 #include "List.h"
 
 // List Member Functions
+List::List() {
+    mHead = NULL;
+    mCount = 0;
+}
+
+List::~List() {
+    if(mHead == NULL) { return; }
+    mHead->previous->next = NULL;
+    Node* deleteNode = mHead;
+    Node* nextNode = mHead->next;
+    delete mHead;
+    while(nextNode != NULL) {
+        deleteNode = nextNode;
+        nextNode = nextNode->next;
+        delete deleteNode;
+    }
+}
+
+Node* List::head() {
+    return mHead;
+}
+
+size_t List::count() {
+    return mCount;
+}
+
+void List::insertAtTail(Node* node) {
+    if(mHead != NULL) {
+        mHead->previous->next = node;
+        node->next = mHead;
+        mHead->previous = node;
+    }
+    else {
+        mHead = node;
+        node->next = node;
+        node->previous = node;
+    }
+}
+
+Node* List::find(std::string key) {
+    if(mHead == NULL) { return NULL; }
+    if(mHead->mKey == key) { return mHead; }
+    Node* nextNode = mHead->next;
+    while(nextNode != mHead) {
+        if(nextNode->mKey == key) { return nextNode; }
+        nextNode = nextNode->next;
+    }
+    return NULL;
+}
+
+void List::remove(std::string key) {
+    Node* removeNode = find(key);
+    if(removeNode == NULL) { return; }
+
+    removeNode->previous->next = removeNode->next;
+    removeNode->next->previous = removeNode->previous;
+    delete removeNode;
+}
--- labs/counter/List.h
+++ labs/counter/List.h
@@ -1,6 +1,34 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <string>
+
 // TODO...
+
+class List {
+  private:
+    Node* mHead;
+    size_t mCount;
+
+  public:
+    List();
+    ~List();
+
+    Node* head();
+    size_t count();
+    void insertAtTail(Node* node);
+    /**
+     * Returns a pointer to the node with key. If not found, returns NULL.
+     */
+    Node* find(std::string key);
+    void remove(std::string key);
+};
+
+struct Node {
+    std::string mKey;
+    int mValue;
+    Node* next;
+    Node* previous;
+};
 
 #endif
