90f9dd827ef0e0c846d1deb1e350a344fe96aa73
Gupperfisher <charlie220@ucsb.edu>
2025-03-20 23:46:16 -0700

    expanded route() function (compiles, but its fucked)

--- labs/bomber/Map.h
+++ labs/bomber/Map.h
@@ -31,7 +31,7 @@ public:
     Map(std::istream& stream);
     ~Map();
 
-    std::string route(Point src, Point dst);
+    std::string route(Point& src, Point& dst);
     void printMap() const;
     void printPerimeter() const;
     void printRegionsFromDisjointSet();
@@ -57,7 +57,8 @@ private:
     Region* getRegion(Tile* t);
     std::array<Point, 4> calculateNeighborPoints(const Point& p);
 
-    vector<Region*> regionPathFinidng(Point& src, Point& dst, int bomb_count, unordered_map<Region*, Region*>& visited_nodes);
+    vector<Region::Connection*> regionPathFinding(Point& src, Point& dst, size_t bomb_count, unordered_map<Region*, Region*>& visited_connections);
+    vector<Tile*> pointPathFinding(Tile* start, Tile* end, size_t& bomb_count, unordered_map<Tile*, Tile*>& visited_tiles);
 };
 
 #endif
